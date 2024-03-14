d6b67598891c6382a6b2d1158f662e02e697838b
Otokami-Orokabu <37827971+Otokami-Orokabu@users.noreply.github.com>
2024-03-14 12:00:59 +0900

    hello world

---
+++ orEngineR2/src/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+/// <summary>
+/// Entry Point
+/// </summary>
+/// <param name="argc"></param>
+/// <param name="argv"></param>
+/// <returns></returns>
+int main(int argc, char** argv)
+{
+    printf_s("hello World!");
+    return 0;
+}
